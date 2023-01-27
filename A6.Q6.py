3072e1319ed9d459f78708103563d44e24083a8e
SiddharthxsrivastavA <120250296+SiddharthxsrivastavA@users.noreply.github.com>
2023-01-27 17:01:51 +0530

    Update A6.Q6.py

--- A6.Q6.py
+++ A6.Q6.py
@@ -7,6 +7,6 @@ def student_data(student_id, **kwargs):
         print(f"Student Class: {kwargs['student_class']}")
 
 
-student_data(student_id='22104074', student_name='Hitesh Kochar')
+student_data(student_id='22104095', student_name='Siddharth Srivastava')
 
-student_data(student_id='22104074', student_name='Hitesh Kochar', student_class='G2')
+student_data(student_id='22104095', student_name='Siddharth Srivastava', student_class='G2')
